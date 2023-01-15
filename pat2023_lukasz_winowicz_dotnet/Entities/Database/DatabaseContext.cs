44233695afc10eb8cda21c36f76c232cf1bc1255
Łukasz Winowicz <lwinowicz@gmail.com>
2023-01-15 09:17:25 +0100

    fixed modelBuilder.Entity (from Book to Author)

--- pat2023_lukasz_winowicz_dotnet/Entities/Database/DatabaseContext.cs
+++ pat2023_lukasz_winowicz_dotnet/Entities/Database/DatabaseContext.cs
@@ -25,7 +25,7 @@ namespace pat2023_lukasz_winowicz_dotnet.Entities.Database
             // BookAuthor
             modelBuilder.Entity<BookAuthor>().HasKey(ba => new {ba.BookId, ba.AuthorId});
             modelBuilder.Entity<BookAuthor>().HasOne(ba => ba.Book).WithMany(b => b.BookAuthors).HasForeignKey(ba => ba.BookId);
-            modelBuilder.Entity<BookAuthor>().HasOne(ba => ba.Book).WithMany(a => a.BookAuthors).HasForeignKey(ba => ba.BookId);
+            modelBuilder.Entity<BookAuthor>().HasOne(ba => ba.Author).WithMany(a => a.BookAuthors).HasForeignKey(ba => ba.AuthorId);
 
             // Author
             modelBuilder.Entity<Author>().HasKey(a => a.Id);
